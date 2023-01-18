d3976cee22bb287fb68539882b029bc9d17e8ff1
winxene <winxeneric@gmail.com>
2023-01-18 17:30:19 +0700

    chore: removed unused code

--- include/firebase.h
+++ include/firebase.h
@@ -1,9 +1,10 @@
 #ifndef __FIREBASE_H__
 #define __FIREBASE_H__
 #include <Firebase_ESP_Client.h>
-//Define FirebaseESP8266 data object
+#include <lvgl.h>
 extern FirebaseData fbdo;
-
+extern bool showQRCode;
+extern lv_obj_t * qrCode;
 void Firebase_Init(const String& streamPath);
 void onFirebaseStream(FirebaseStream data);
 void onFirebaseStreamTimeout(bool timeout);
