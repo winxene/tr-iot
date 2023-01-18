a97b20e827943185ed03fbeebc3c6c8e62f4df6c
winxene <winxeneric@gmail.com>
2023-01-18 17:29:20 +0700

    chore: change pin

--- include/dispenser.h
+++ include/dispenser.h
@@ -1,9 +1,9 @@
 #include <NewPing.h>
 
-#define TRIGGER_PIN  2  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     4  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define TRIGGER_PIN 14  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN    13// Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-const int  dpm = 22;
+const int  dpm = 21;
 
 void pumpLogic(bool pump);
 
