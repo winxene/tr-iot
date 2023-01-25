7f0ca50ca3304bbcd777697f62ff46cefd4a6f4f
winxene <winxeneric@gmail.com>
2023-01-25 23:59:25 +0700

    chore: retuning pump

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include "MyDisplay.h"
+#include "dispenser.h"
 #include "firebase.h"
 #include "wifiConfig.h"
-#include "dispenser.h"
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.//    
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);  // NewPing setup of pins and maximum distance.//
 
-lv_obj_t * mainScreen;
-lv_obj_t * qrCode; 
+lv_obj_t *mainScreen;
+lv_obj_t *qrCode;
 lv_obj_t *logoImage;
-lv_obj_t * startButton;
-char text [255];
+lv_obj_t *startButton;
+char text[255];
 
 const char *ssid = "hi";
 const char *password = "senpai8668";
@@ -22,96 +22,89 @@ long elapsedTime = 0;
 unsigned long timeCounter = 0;
 
 // long targetTime = 268;
-long targetTime = 77; //for 100ml
+long targetTime = 150;  // for 100ml
 bool sistem = false;
 
 LV_IMG_DECLARE(drinkify);
 
-void pumpSetup()
-{
+void pumpSetup() {
   pinMode(dpm, OUTPUT);
   digitalWrite(dpm, HIGH);
 }
 
-void pumpLogic(bool pump)
-{
-  //HIGH= off, LOW= on
-  while(pump){
+void pumpLogic(bool pump) {
+  // HIGH= off, LOW= on
+  while (pump) {
     delay(50);
     int cm = sonar.ping_cm();
-    Serial.printf("cm: %d\n",cm);
-    if (cm > 1 && cm < 6){
+    Serial.printf("cm: %d\n", cm);
+    if (cm > 1 && cm < 6) {
       timeCounter++;
-      Serial.printf("on timer: %d\n",timeCounter);
+      Serial.printf("on timer: %d\n", timeCounter);
       digitalWrite(dpm, LOW);
       // digitalWrite(dpm, HIGH);
-      if(timeCounter>=targetTime){
+      if (timeCounter >= targetTime) {
         digitalWrite(dpm, HIGH);
         // digitalWrite(dpm, LOW);
         Serial.println("done~!");
         pushToFirebase("/D-1", " ");
-        timeCounter=0;
+        timeCounter = 0;
         pump = false;
         ESP.restart();
         break;
       }
       delay(10);
-    }
-    else digitalWrite(dpm, HIGH);
+    } else
+      digitalWrite(dpm, HIGH);
   }
 }
 
-void displayFlush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
-{
-  uint32_t w = ( area->x2 - area->x1 + 1 );
-  uint32_t h = ( area->y2 - area->y1 + 1 );
+void displayFlush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
 
   myDisplay.startWrite();
-  myDisplay.setAddrWindow( area->x1, area->y1, w, h );
+  myDisplay.setAddrWindow(area->x1, area->y1, w, h);
   myDisplay.writePixels((lgfx::rgb565_t *)&color_p->full, w * h);
   myDisplay.endWrite();
   lv_disp_flush_ready(disp);
 }
 
-void readTouchPad( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
-{
-    uint16_t touchX, touchY;
-    bool touched = myDisplay.getTouch(&touchX, &touchY);
-    if (!touched)
-    {
-      data->state = LV_INDEV_STATE_REL;
-    }
-    else
-    {
-      data->state = LV_INDEV_STATE_PR;
-      /*Set the coordinates*/
-      data->point.x = touchX;
-      data->point.y = touchY;
-    }
+void readTouchPad(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
+  uint16_t touchX, touchY;
+  bool touched = myDisplay.getTouch(&touchX, &touchY);
+  if (!touched) {
+    data->state = LV_INDEV_STATE_REL;
+  } else {
+    data->state = LV_INDEV_STATE_PR;
+    /*Set the coordinates*/
+    data->point.x = touchX;
+    data->point.y = touchY;
+  }
 }
 
-//taken from https://codereview.stackexchange.com/questions/29198/random-string-generator-in-c
-void generateString(size_t size){
+// taken from https://codereview.stackexchange.com/questions/29198/random-string-generator-in-c
+void generateString(size_t size) {
   char stringResult[255];
-  static const char alphaNumeric[]=         
-        "0123456789"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
-  if(size){
+  static const char alphaNumeric[] =
+      "0123456789"
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      "abcdefghijklmnopqrstuvwxyz";
+  if (size) {
     --size;
-    size_t increment =0;
-    while(increment<size){
-      int key = rand() % (int) (sizeof alphaNumeric - 1);
+    size_t increment = 0;
+    while (increment < size) {
+      int key = rand() % (int)(sizeof alphaNumeric - 1);
       stringResult[increment] = alphaNumeric[key];
       increment++;
     }
-    stringResult[size] = '\0'; // \0 indicates end string.
+    stringResult[size] = '\0';  // \0 indicates end string.
   }
   Serial.println(stringResult);
   strcpy(text, stringResult);
 }
 
-void generateQRCode(lv_event_t * e){
+void generateQRCode(lv_event_t *e) {
   lv_event_code_t code = lv_event_get_code(e);
   if (code == LV_EVENT_CLICKED || code == LV_EVENT_FOCUSED) {
     generateString(17);
@@ -119,16 +112,15 @@ void generateQRCode(lv_event_t * e){
     lv_obj_clear_flag(qrCode, LV_OBJ_FLAG_HIDDEN);
     pushToFirebase("/D-1", text);
     // bool showQRCode=true;
-    int clock= 157;
-    int counter=0;
-    while(showQRCode){
+    int clock = 157;
+    int counter = 0;
+    while (showQRCode) {
       counter++;
       delay(50);
-      if(counter>=clock)
-      {
+      if (counter >= clock) {
         lv_obj_add_flag(qrCode, LV_OBJ_FLAG_HIDDEN);
-        counter=0;
-        showQRCode=false;
+        counter = 0;
+        showQRCode = false;
         break;
       }
       delay(10);
@@ -136,9 +128,8 @@ void generateQRCode(lv_event_t * e){
   }
 }
 
-
-void titleLabel(){  
-  lv_obj_t * titleLabel = lv_label_create(mainScreen);
+void titleLabel() {
+  lv_obj_t *titleLabel = lv_label_create(mainScreen);
   lv_obj_set_pos(titleLabel, 0, 150);
   lv_obj_set_align(titleLabel, LV_ALIGN_TOP_MID);
   lv_label_set_text(titleLabel, "Tap Start to Generate QR-Code");
@@ -148,8 +139,8 @@ void titleLabel(){
 
 void ui_background() {
   mainScreen = lv_obj_create(NULL);
-  lv_obj_clear_flag(mainScreen, LV_OBJ_FLAG_SCROLLABLE); //cannot scroll
-  logoImage = lv_img_create(mainScreen); 
+  lv_obj_clear_flag(mainScreen, LV_OBJ_FLAG_SCROLLABLE);  // cannot scroll
+  logoImage = lv_img_create(mainScreen);
   lv_img_set_src(logoImage, &drinkify);
   lv_obj_set_size(logoImage, 177, 68);
   lv_obj_set_pos(logoImage, 0, 20);
@@ -160,14 +151,14 @@ void ui_background() {
 }
 
 void ui_start_button() {
-  lv_obj_t * label;
+  lv_obj_t *label;
   startButton = lv_btn_create(mainScreen);
   lv_obj_add_event_cb(startButton, generateQRCode, LV_EVENT_ALL, NULL);
-  
-  lv_obj_set_size(startButton, 120, 50);  
+
+  lv_obj_set_size(startButton, 120, 50);
   lv_obj_align(startButton, LV_ALIGN_BOTTOM_MID, 0, -10);
   lv_obj_clear_flag(startButton, LV_OBJ_FLAG_HIDDEN);
- 
+
   label = lv_label_create(startButton);
   lv_obj_set_style_text_font(label, &lv_font_montserrat_14, LV_PART_MAIN | LV_STATE_DEFAULT);
   lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
@@ -188,8 +179,7 @@ void ui_init() {
   lv_disp_load_scr(mainScreen);
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   pumpSetup();
   WiFiConnect();
@@ -198,7 +188,7 @@ void setup()
   myDisplay.init();
   myDisplay.setBrightness(255);
   lv_init();
-  lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * 10 );
+  lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * 10);
 
   static lv_disp_drv_t disp_drv;
   lv_disp_drv_init(&disp_drv);
@@ -215,7 +205,7 @@ void setup()
   indev_drv.read_cb = readTouchPad;
   lv_indev_drv_register(&indev_drv);
   ui_init();
-  
+
   Firebase_Init("/D-1/phoneData/status");
   pushToFirebase("/D-1", " ");
 }
@@ -223,4 +213,3 @@ void loop() {
   lv_timer_handler();
   delay(5);
 }
-
